081addca7014658790a1d09a2368d231e32a5c57
Zehna7 <zehna.omar2012@gmail.com>
2024-10-17 00:23:31 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+v = "13/5/2011"
+w = "19/6/2009"
+x = "8/2/2008"
+y = "24/11/2010"
+z = "30/8/2012"
+print("My friend Sarah's Birthday is on",v)
+print("My friend John's Birthday is on",w)
+print("My friend Brooklyn's Birthday is on",x)
+print("My friend Vanessa's Birthday is on",y)
+print("My Birthday is on",z)
